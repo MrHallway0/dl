fb961bfbdcb04e4a60cae564a8d0e970507f8fc6
Mr. Hallway <git@mrhallway.me>
2021-05-11 19:49:20 -0700

    Moved URL to /index.html

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function onClick() {
     var query = document.getElementById("input").value;
     console.log('Trying to go to ' + query)
-    location.replace(query)
+    location.replace(query + '/index.html')
     console.log('Oh no, you should not be seeing this!')
 }
