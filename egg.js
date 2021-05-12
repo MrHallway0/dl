7d594d830c3a8ee989b2dccd20d220c0873144e9
Mr. Hallway <git@mrhallway.me>
2021-05-11 19:34:43 -0700

    Updated indexs

---
+++ egg.js
@@ -0,0 +1,6 @@
+console.log('      _');
+console.log('     |-|  __');
+console.log('jgs  |=| [Ll]');
+console.log('     "^" ====`o');
+console.log('');
+console.log('Hello there!');
--- scripts/input.js
+++
@@ -1,4 +0,0 @@
-function onClick() {
-    var query = document.getElementById("input").value;
-    location.replace(window.location.href + query)
-}
